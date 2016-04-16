5dc4e860f87c99748ccaf307da86535bc1f7660b
Lyle Franklin <lylejfranklin@gmail.com>
2016-04-16 11:16:08 -0700

    Use correct terraform map output syntax

--- fixtures/aws/outputs.tf
+++ fixtures/aws/outputs.tf
@@ -1,6 +1,6 @@
 output "vpc_id" {
     value = "${aws_vpc.test_vpc.id}"
 }
-output "vpc_tags" {
-    value = "${aws_vpc.test_vpc.tags}"
+output "vpc_tag_name" {
+    value = "${aws_vpc.test_vpc.tags.Name}"
 }
