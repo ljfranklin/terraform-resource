3d3eb7a52b1824977f6be3be23cd28e34b9b33bd
Lyle Franklin <lylejfranklin@gmail.com>
2016-05-08 17:02:54 -0700

    Remove duplicate test

--- out/out_test.go
+++ out/out_test.go
@@ -161,32 +161,6 @@ var _ = Describe("Out", func() {
 		assertOutBehavior(req, expectedMetadata)
 	})
 
-	It("creates IaaS resources from a local terraform source", func() {
-		req := models.OutRequest{
-			Source: models.Source{
-				Storage: storageModel,
-			},
-			Params: models.Params{
-				EnvName: envName,
-				Terraform: terraform.Model{
-					Source: "fixtures/aws/",
-					Vars: map[string]interface{}{
-						"access_key":  accessKey,
-						"secret_key":  secretKey,
-						"vpc_id":      vpcID,
-						"subnet_cidr": subnetCIDR,
-					},
-				},
-			},
-		}
-		expectedMetadata := map[string]interface{}{
-			"vpc_id":      vpcID,
-			"subnet_cidr": subnetCIDR,
-			"tag_name":    "terraform-resource-test", // template default
-		}
-
-		assertOutBehavior(req, expectedMetadata)
-	})
 	It("creates IaaS resources from `source.terraform.vars`", func() {
 		req := models.OutRequest{
 			Source: models.Source{
