89b5589867f5d1398836f2a26843c52bd4114b15
Lyle Franklin <lfranklin@pivotal.io>
2016-10-16 13:06:52 -0700

    Add object_content as output
    - Fixes destroy stages in CI

--- fixtures/aws/outputs.tf
+++ fixtures/aws/outputs.tf
@@ -7,6 +7,9 @@ output "bucket" {
 output "object_key" {
     value = "${aws_s3_bucket_object.s3_object.id}"
 }
+output "object_content" {
+    value = "${var.object_content}"
+}
 output "content_md5" {
     value = "${aws_s3_bucket_object.s3_object.etag}"
 }
--- fixtures/module/outputs.tf
+++ fixtures/module/outputs.tf
@@ -7,6 +7,9 @@ output "bucket" {
 output "object_key" {
     value = "${module.test_module.object_key}"
 }
+output "object_content" {
+    value = "${module.test_module.object_content}"
+}
 output "content_md5" {
     value = "${module.test_module.content_md5}"
 }
