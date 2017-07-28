2bac7f587c85f96975a02515c5f5309e92bc0a7f
Divya Bhargov <dbhargov@pivotal.io>
2017-07-28 14:30:46 -0700

    Add build information as terraform variables
    Some build information like build_id, pipeline_name, etc are available
    as environment variables in container environment. These are exposed
    as terraform variables
    
    More details in https://github.com/ljfranklin/terraform-resource/issues/36

--- fixtures/module/module.tf
+++ fixtures/module/module.tf
@@ -6,6 +6,14 @@ module "test_module" {
   region = "${var.region}"
   env_name = "${var.env_name}"
 
+  env_name = "${var.env_name}"
+  build_id = "${var.build_id}"
+  build_name = "${var.build_name}"
+  build_job_name = "${var.build_job_name}"
+  build_pipeline_name = "${var.build_pipeline_name}"
+  build_team_name = "${var.build_team_name}"
+  atc_external_url = "${var.atc_external_url}"
+
   bucket = "${var.bucket}"
   object_key = "${var.object_key}"
   object_content = "${var.object_content}"
