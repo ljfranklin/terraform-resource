9e236da04f7e51b30226e430e32ccfae387ea231
Lyle Franklin <lylejfranklin@gmail.com>
2024-12-09 08:48:52 -0500

    Fix ephemeral var validation error in fixtures
    ```
    │ The variable build_job_name cannot be set using the -var and -var-file
    │ options when applying a saved plan file, because a saved plan includes the
    │ variable values that were set when it was created. The saved plan specifies
    │ "apply-plan" as the value whereas during apply the value "create-plan" was
    │ set by an environment variable. To declare an ephemeral variable which is
    │ not saved in the plan file, use ephemeral = true.
    ```

--- fixtures/aws/variables.tf
+++ fixtures/aws/variables.tf
@@ -4,12 +4,24 @@ variable "region" {
     default = "us-east-1"
 }
 variable "env_name" {}
-variable "build_id" {}
-variable "build_name" {}
-variable "build_job_name" {}
-variable "build_pipeline_name" {}
-variable "build_team_name" {}
-variable "atc_external_url" {}
+variable "build_id" {
+    ephemeral = true
+}
+variable "build_name" {
+    ephemeral = true
+}
+variable "build_job_name" {
+    ephemeral = true
+}
+variable "build_pipeline_name" {
+    ephemeral = true
+}
+variable "build_team_name" {
+    ephemeral = true
+}
+variable "atc_external_url" {
+    ephemeral = true
+}
 
 variable "bucket" {}
 variable "object_key" {}
